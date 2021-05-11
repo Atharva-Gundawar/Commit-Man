ad6a716a97f449a2ecf1854ede23cca00b4aa272
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-11 23:24:44 +0530

    Feat : Get commit Diff

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import difflib
 
 def get_files(directory = os.curdir):
     files =[]
@@ -6,3 +7,16 @@ def get_files(directory = os.curdir):
         for name in files:
             files.append(os.path.join(path, name))
     return files
+
+def get_commit_diff(cm_file_path,file_path):
+    file_diff = []
+    with open('/tmp/hosts0', 'r') as hosts0:
+        with open('/tmp/hosts1', 'r') as hosts1:
+            diff = difflib.unified_diff(
+                hosts0.readlines(),
+                hosts1.readlines(),
+                fromfile='hosts0',
+                tofile='hosts1',
+            )
+            for line in diff:
+                file_diff(line)
