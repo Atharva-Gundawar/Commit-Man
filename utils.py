a9f7a9e833cdd7250811278f63f4d93c798ae518
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-11 23:36:18 +0530

    Fix : Get commit Diff

--- utils.py
+++ utils.py
@@ -10,13 +10,13 @@ def get_files(directory = os.curdir):
 
 def get_commit_diff(cm_file_path,file_path):
     file_diff = []
-    with open('/tmp/hosts0', 'r') as hosts0:
-        with open('/tmp/hosts1', 'r') as hosts1:
+    with open(cm_file_path, 'r') as hosts0:
+        with open(file_path, 'r') as hosts1:
             diff = difflib.unified_diff(
                 hosts0.readlines(),
                 hosts1.readlines(),
-                fromfile='hosts0',
-                tofile='hosts1',
+                fromfile='old',
+                tofile='new',
             )
             for line in diff:
                 file_diff(line)
