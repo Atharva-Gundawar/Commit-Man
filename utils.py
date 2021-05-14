b2710bb4cda90f3a52d8e323417430a7f1414f8f
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-14 23:53:35 +0530

    Fix : Spacings

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import os
 import difflib
 
-def get_files(directory = os.curdir):
-    files =[]
+def get_files(directory=os.curdir):
+    files = []
     for path, subdirs, files in os.walk(directory):
         for name in files:
             files.append(os.path.join(path, name))
