4a856735efe57065d2572cd49cc153371a6e1104
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-14 23:48:06 +0530

    Fix : Commit fn

--- utils.py
+++ utils.py
@@ -30,18 +30,7 @@ commit number => num
 commit msg => msg
 """
 
-def commit():
-# def commit(dir_path,cm_dir,messgae):
-    list_of_dirs = []
-    # for path, subdirs, files in os.walk("C:/Users/Atharva"):
-    #     print(path)
-        # for name in path:
-            # print(name)
-    for name in os.listdir("C:/Users/Atharva"):
-        # print(name)
-        if os.path.isdir(os.path.abspath(name)):
-            print(name)
-            list_of_dirs.append(os.path.abspath(name))
-    for dir in list_of_dirs:
-        print(dir)
-commit()
+def commit(dir_path,cm_dir,messgae):
+    list_subfolders_with_paths = [f.path for f in os.scandir(dir_path) if f.is_dir()]
+    print(list_subfolders_with_paths)
+
