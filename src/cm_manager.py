0dfbb002965d1d5115ae0e3c64a718ddaf64ff6e
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-19 19:17:46 +0530

    Feat : Relocted commit diff fn

--- src/cm_manager.py
+++ src/cm_manager.py
@@ -34,29 +34,6 @@ Log file structure:
 
 """
 
-
-def get_commit_diff(cm_file_path,file_path):
-    """
-    Prints line by line diff for 2 versions of a file .
-
-    @param cm_file_path: Old version of the file
-    @param file_path: New version of the file
-
-    """
-    # cm_file_path is the path of the file saved inside the .cm folder.
-    # file_path is the path of the file in the active directory.
-    file_diff = []
-    with open(cm_file_path, 'r') as hosts0:
-        with open(file_path, 'r') as hosts1:
-            diff = difflib.unified_diff(
-                hosts0.readlines(),
-                hosts1.readlines(),
-                fromfile='old',
-                tofile='new',
-            )
-            for line in diff:
-                file_diff(line)
-            
 def commit(dir_path,msg):
     """
     Commits latest version of the directory into a new dir 
--- src/file_system_utils.py
+++ src/file_system_utils.py
@@ -43,3 +43,27 @@ def copytree(src, dst, symlinks=False, ignore=None):
             shutil.copytree(s, d, symlinks, ignore)
         else:
             shutil.copy2(s, d)
+
+
+def get_commit_diff(cm_file_path,file_path):
+    """
+    Prints line by line diff for 2 versions of a file .
+
+    @param cm_file_path: Old version of the file
+    @param file_path: New version of the file
+
+    """
+    # cm_file_path is the path of the file saved inside the .cm folder.
+    # file_path is the path of the file in the active directory.
+    file_diff = []
+    with open(cm_file_path, 'r') as hosts0:
+        with open(file_path, 'r') as hosts1:
+            diff = difflib.unified_diff(
+                hosts0.readlines(),
+                hosts1.readlines(),
+                fromfile='old',
+                tofile='new',
+            )
+            for line in diff:
+                file_diff(line)
+            
