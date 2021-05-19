cfa89da4ccf4ca8f41767fb1a07c429b17b3da00
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-19 19:14:04 +0530

    Feat : Tree utils

--- src/utils.py
+++ src/utils.py
@@ -35,51 +35,6 @@ Log file structure:
 """
 
 
-def compare_trees(dir1, dir2):
-    """
-    Compare two directories recursively. Files in each directory are
-    assumed to be equal if their names and contents are equal.
-
-    @param dir1: First directory path
-    @param dir2: Second directory path
-
-    @return: True if the directory trees are the same and 
-        there were no errors while accessing the directories or files, 
-        False otherwise.
-    """
-
-    dirs_cmp = filecmp.dircmp(dir1, dir2)
-    if len(dirs_cmp.left_only)>0 or len(dirs_cmp.right_only)>0 or len(dirs_cmp.funny_files)>0:
-        return False
-    (_, mismatch, errors) =  filecmp.cmpfiles(
-        dir1, dir2, dirs_cmp.common_files, shallow=False)
-    if len(mismatch)>0 or len(errors)>0:
-        return False
-    for common_dir in dirs_cmp.common_dirs:
-        new_dir1 = os.path.join(dir1, common_dir)
-        new_dir2 = os.path.join(dir2, common_dir)
-        if not compare_trees(new_dir1, new_dir2):
-            return False
-    return True
- 
-def copytree(src, dst, symlinks=False, ignore=None):
-    """
-    Copy a directory tree to another location.
-    It ignores copying if .cm folder is found
-    in its path
-
-    @param src: source directory path
-    @param dst: destination directory path
-
-    """
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s) and '.cm' not in s:
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
-
 def get_commit_diff(cm_file_path,file_path):
     """
     Prints line by line diff for 2 versions of a file .
