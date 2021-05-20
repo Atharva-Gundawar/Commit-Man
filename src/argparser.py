12398723ece05895ece03419303d3bdc7588965e
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-20 23:50:29 +0530

    Feat : ArgParse Class

--- src/argparser.py
+++ src/argparser.py
@@ -1,6 +1,11 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Welcome to COMMIT MAN')
+class GetArgumentParser:
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='Welcome to COMMIT MAN')
+        parser.add_argument("-f", "--force",type=str,help="For Force mode")
+        parser.add_argument("-p", "--path", type=str,help="Path to target directory")
+        self.parser = parser
 
-
-args = parser.parse_args()
+    def getParser(self):
+        return self.parser
