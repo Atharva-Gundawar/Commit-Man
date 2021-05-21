98f8e5fba98d8998148e2d8a5159f223590192d8
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-21 23:42:17 +0530

    Fix : Argument Parsing

--- src/main.py
+++ src/main.py
@@ -6,17 +6,15 @@ def main():
     argparse = GetArgumentParser()
     arguments = argparse.getArguments()
     cur_dir = os.path.abspath(os.path.curdir)
-    if arguments['cm']:
-        if arguments['init']:
-            CommitMan.init(cur_dir)
-        elif arguments['commit']:
-            CommitMan.commit(cur_dir,arguments['<message>'])
-        elif arguments['revert']:
-            CommitMan.revert(arguments['<number>'],cur_dir,arguments['--force'])
-        else:
-            print(argparse.doc)
+    if arguments['init']:
+        CommitMan.init(cur_dir)
+    elif arguments['commit']:
+        CommitMan.commit(cur_dir,arguments['<message>'])
+    elif arguments['revert']:
+        CommitMan.revert(arguments['<number>'],cur_dir,arguments['--force'])
     else:
         print(argparse.doc)
+
 
 if __name__ == '__main__' :
     main()
