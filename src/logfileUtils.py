79942a16145178d89478f1a253de28f7960a4635
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-21 20:35:12 +0530

    Fix : Log utils

--- src/logfileUtils.py
+++ src/logfileUtils.py
@@ -1,48 +1,46 @@
 import os
 import sys
 import sqlite3
+import datetime
 
-class LogUtils:
-    @staticmethod
-    def msgAndNumCheck(msg, num):
-        """
-        Checks Commit Message & Commit Number are in the correct format 
-        
-        @param msg: Commit Message
-        @param num: Commit Number
-
-        @return Commit Message & Commit Number are in the correct format
-                else returns False
-
-        """
-        return(isinstance(msg, str) and isinstance(num,str)
-            and len(msg) > 0 and len(msg) < 128
-            and int(num) > 0
-            and msg is not None and num is not None
-            )
+def msgAndNumCheck(msg, num):
+    """
+    Checks Commit Message & Commit Number are in the correct format 
     
-    @staticmethod
-    def updateLogfile(cm_dir,msg,num):
-        """
-        Updates log file with Commit Message, Commit Number and Datetime
-        
-        @param cm_dir : Path to the Commit Man Directory 
-        @param msg: Commit Message
-        @param num: Commit Number
+    @param msg: Commit Message
+    @param num: Commit Number
 
-        """
+    @return Commit Message & Commit Number are in the correct format
+            else returns False
 
-        if msgAndNumCheck(msg, num):
-            if os.path.exists(os.path.join(cm_dir,'log.db')):
-                    try:
-                        con = sqlite3.connect(os.path.join(cm_dir,'log.db'))
-                        cur = con.cursor()
-                        cur.execute(f'''INSERT INTO log (message, number, datetime ) VALUES({msg}, {num+1},datetime('now', 'localtime'))''')
-                        con.commit()
-                        con.close()
-                    except Exception as e:
-                        raise Exception(f'SQL database could not be updated : {e}')
-            else:
-                raise Exception('Cannot find log file')
+    """
+    return(isinstance(msg, str) and isinstance(num,int)
+        and len(msg) > 0 and len(msg) < 128
+        and int(num) > 0
+        and msg is not None and num is not None
+        )
+
+def updateLogfile(cm_dir,msg,num):
+    """
+    Updates log file with Commit Message, Commit Number and Datetime
+    
+    @param cm_dir : Path to the Commit Man Directory 
+    @param msg: Commit Message
+    @param num: Commit Number
+
+    """
+    if msgAndNumCheck(msg, num):
+        if os.path.exists(os.path.join(cm_dir,'log.db')):
+                try:
+                    con = sqlite3.connect(os.path.join(cm_dir,'log.db'))
+                    cur = con.cursor()
+                    sql = '''INSERT INTO log (message, number, datetime ) VALUES(?,?,datetime('now', 'localtime'))'''
+                    cur.execute(sql,[msg,num,])
+                    con.commit()
+                    con.close()
+                except Exception as e:
+                    raise Exception(f'SQL database could not be updated : {e}')
         else:
-            raise Exception('Check commit msg')
+            raise Exception('Cannot find log file')
+    else:
+        raise Exception('Check commit msg')
