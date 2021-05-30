c049a27c410cd32740d5799fd8e95fdb419694ed
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-31 01:18:53 +0530

    Move : Commands

--- src/main.py
+++ src/main.py
@@ -1,9 +1,54 @@
 from src.argparser import GetArgumentParser
 from src.logfileUtils import LogUtils
 from src.cmManager import CommitMan
-import src.manPages 
 import sys
 import os
+
+commands = {
+    'init' : """
+Initialize Commit man in the current directory
+
+Usage : cm init
+
+This will create a .cm folder in the current directory,
+and a log file inside that folder.
+""",
+
+    "reinit" : """
+Reinitialize Commit man in the current directory
+
+Usage : cm reinit
+
+This will reinitialize .cm folder in case of
+logfile corruption or unavailability.
+""",
+    
+    "commit" : """
+Commits curent version of working directory
+
+Usage : cm commit <message>
+
+This will create a new commit folder insider the .cm folder.
+""",
+    "revert" : """
+Reverts to an old version of working directory
+
+Usage : cm revert <Commit_Number> [-f | --force]
+
+This revert to an older version of the project and with the 
+force option revert will take place even if the latest code 
+has not been commited.
+""",
+    "showlog" : """
+Displays Log file in a tabular format on the Terminal
+
+Usage : cm showlog
+
+Queries data from the log file and adds headers and spacing,
+then displays them to the terminal.
+
+"""
+}
 
 def main():
     """
@@ -28,8 +73,8 @@ def main():
         print("\n")
         print("#"*60)
         print('-'*60)
-        for command in manPages.commands.keys():
-            print(f'\n{command}:\n{manPages.commands[command]}')
+        for command in commands.keys():
+            print(f'\n{command}:\n{commands[command]}')
             print('-'*60)
         print("#"*60,"\n")
 
